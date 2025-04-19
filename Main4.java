964c73edf675c85940a128f07319c263643dc66b
Anushree <gaulkaranushri@gmail.com>
2025-04-19 23:20:19 +0530

    Circle

---
+++ Main4.java
@@ -0,0 +1,15 @@
+// File: Main.java
+
+
+
+public class Main4 {
+    public static void main(String[] args) {
+        // Create a Circle object
+        Circle c1 = new Circle(5);
+
+        // Display area and perimeter
+        System.out.println("Circle Area: " + c1.area());
+        System.out.println("Circle Perimeter: " + c1.perimeter());
+    }
+}
+
